7c3faf0b75cd1aa26c54203bbcbd871b3704798a
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-07 11:42:48 -0300

    Aula 04 - Adequa dados do servidor Restful para o do servidor Cliente

--- restful-server/utils/validator.js
+++ restful-server/utils/validator.js
@@ -1,23 +1,16 @@
 module.exports = {
 
-    user:(app, req, res)=>{
-
-        req.assert('name', 'O nome é obrigatório.').notEmpty();
-        req.assert('email', 'O e-mail está inválido.').notEmpty().isEmail();
+    user: (app, req, res) => {
+        req.assert('_name', 'O nome é obrigatório.').notEmpty();
+        req.assert('_email', 'O e-mail está inválido.').notEmpty().isEmail();
 
         let errors = req.validationErrors();
 
         if (errors) {
-
             app.utils.error.send(errors, req, res);
             return false;
-
         } else {
-
             return true;
-
         }
-
     }
-
 };
